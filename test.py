2742104b041c6ca66ec42b0d1b13f25e6b071602
Zhicong Zhang <zhangzhicong@wildroi.com>
2018-06-13 15:14:30 +0800

    create

---
+++ test.py
@@ -0,0 +1,10 @@
+# print(100 + 200 + 300)
+# name = input('please enter your name: ')
+# print('hello,', name)
+
+# def f(x):
+# 	return x * x
+# r = list(map(f, range(1,10)))
+# r = list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+# print(r)
+
